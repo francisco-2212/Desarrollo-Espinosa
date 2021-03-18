376c919d262c45940355619673d4dbb51b14b38d
francisco-2212 <80895215+francisco-2212@users.noreply.github.com>
2021-03-18 08:55:40 -0300

    Add files via upload

---
+++ ejercico1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desarrollo.espinosa;
+
+/**
+ *
+ * @author pc07
+ */
+public class ejercico1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a= 4;
+        int b=7;
+        int res;
+        res= a+b;
+        System.out.println("resultado es" +res);
+        
+    }
+    
+}
