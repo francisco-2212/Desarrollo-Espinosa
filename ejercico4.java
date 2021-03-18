99bad3826d6484666949eee4490fdcd21c29a05b
francisco-2212 <80895215+francisco-2212@users.noreply.github.com>
2021-03-18 10:09:22 -0300

    Add files via upload

---
+++ ejercico4.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desarrollo.espinosa;
+import java.util.Scanner;
+/**
+ *
+ * @author pc07
+ */
+public class ejercico4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        String nombredeltrabajador;
+        double costodehoras;
+        double cantidaddehorastrabajadaspormes;
+        double salario;
+        System.out.println("nombre del trabajador");
+        nombredeltrabajador=entrada.nextLine();
+        
+        System.out.println("costo de horas $");
+        costodehoras=entrada.nextDouble();
+        
+        System.out.println("cantidad de horas por mes");
+        cantidaddehorastrabajadaspormes=entrada.nextDouble();
+        
+        salario=(costodehoras*cantidaddehorastrabajadaspormes);
+        System.out.println("el salario MENSUAL es"+salario);
+        
+        
+        
+        
+        
+        
+    }
+    
+}
