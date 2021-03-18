2d0c6d86e9808916a8dfb3223eea300977dc08ed
francisco-2212 <80895215+francisco-2212@users.noreply.github.com>
2021-03-18 09:20:50 -0300

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desarrollo.espinosa;
+
+/**
+ *
+ * @author pc07
+ */
+public class ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int suma= a+b;
+        int resta= a-b;
+        int multiplicacion= a*b;
+        int division= a/b;
+        System.out.println("la suma de" +a+" mas" +b+ "es igual a" +suma);
+        System.out.println("la resta de" +a+ "menos" +b+ "es igual a" +resta);
+        System.out.println("la multiplicacion de" +a+ "por" +b+ "es igual a" +multiplicacion);
+        System.out.println("la division de" +a+ "dividido" +b+ "es igual a" +division);
+        
+        
+    }
+    
+}
